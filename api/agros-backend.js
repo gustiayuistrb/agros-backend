d98e063d36d612a101aafa403ba2da4d41af737c
gustiayuistrb <istiaraayu8@gmail.com>
2024-12-17 17:43:20 +0700

    updatee initial

--- api/agros-backend.js
+++ api/agros-backend.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('../routes/authRoutes');
+const userRoutes = require('../routes/userRoutes');
 const serverless = require('serverless-http');
 
 const app = express();
